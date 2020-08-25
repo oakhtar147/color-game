ef1bf993931a09eb26a60143ef5a12e1edfd3b78
oakhtar147 <osamaakhtar15@gmail.com>
2020-08-25 23:30:45 +0500

    refining frontend, some bugs left

--- index.js
+++ index.js
@@ -60,7 +60,61 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum and the minimum are inclusive
-}    
+}   
+
+function resetColors(){
+    if (numSquares === 6){
+        h1.style.backgroundColor = h1Color;
+        easy.removeAttribute("style");
+        hard.removeAttribute("style");
+        option.removeAttribute("style");
+        easy.classList.remove("selected");
+        hard.classList.add("selected");
+        status.style.color = "white";
+    } else if (numSquares === 3){
+        easy.removeAttribute("style");
+        hard.removeAttribute("style");
+        option.removeAttribute("style");
+        h1.style.backgroundColor = h1Color; 
+        easy.classList.add("selected");
+        hard.classList.remove("selected");
+        status.style.color = "white";
+    }
+} 
+
+function boxEvents(number){
+    if (number === 6){
+        boxes.forEach(function(box){ !losingColors.includes(box) ? box.style.backgroundColor = winColor : box.style.backgroundColor = "#232323"});
+        h1.style.background = winColor;
+        status.textContent = " Correct! ";
+        option.textContent = "Play Again?";
+        status.style.color = winColor;
+        option.style.color = winColor;
+        easy.style.color = winColor;
+        hard.style.backgroundColor = winColor; 
+    } else if (number === 3){
+        boxes.forEach(function(box){ !losingColors.includes(box) ? box.style.backgroundColor = winColor : box.style.backgroundColor = "#232323"});
+        h1.style.background = winColor;
+        status.textContent = " Correct! ";
+        option.textContent = "Play Again?";
+        status.style.color = winColor;
+        option.style.color = winColor;
+        hard.style.color = winColor;
+        easy.style.backgroundColor = winColor; 
+    }    
+}
+
+function hoverEffect(element){
+    element.onmouseover = function(){
+        this.style.color = "white";
+        this.style.backgroundColor = winColor;
+    }
+    element.onmouseleave = function(){
+        this.style.backgroundColor = "white";
+        this.style.color = winColor;
+    }
+
+}
 
 option.textContent = "New Colors";
 option.addEventListener("click", function(){
@@ -70,7 +124,7 @@ option.addEventListener("click", function(){
     updateBoxColors(); 
     [r, g, b] = getRGBValues();
     updateH1Tag();
-    h1.style.backgroundColor = h1Color; 
+    resetColors();
     losingColors = []; // once  we click the button we want no colors in our losingColors array       
 })
 
@@ -82,6 +136,7 @@ easy.addEventListener("click", function(){
     hard.classList.remove("selected");
     h1.style.backgroundColor = h1Color;
     status.textContent = "Try Again!";
+    option.textContent = "New Colors";
     status.style.color = "white";   
     numSquares = 3;
     colors = generateRandomColors(numSquares);
@@ -89,12 +144,14 @@ easy.addEventListener("click", function(){
     [r, g, b] = getRGBValues();
     updateH1Tag();
     updateBoxColors();
+    resetColors();
 })
 
 hard.addEventListener("click", function(){
     hard.classList.add("selected");
     easy.classList.remove("selected");
     h1.style.backgroundColor = h1Color;
+    option.textContent = "New Colors";
     status.textContent = "Try Again!";
     status.style.color = "white";       
     numSquares = 6;
@@ -103,6 +160,8 @@ hard.addEventListener("click", function(){
     [r, g, b] = getRGBValues();
     updateH1Tag();
     updateBoxColors();
+    resetColors();
+
 })
 
 // events
@@ -112,10 +171,9 @@ let losingColors = [];
 for(let i=0; i<boxes.length; i++){
     boxes[i].addEventListener("click", function(){
         if (this.style.backgroundColor === winColor){
-            boxes.forEach(function(box){ !losingColors.includes(box) ? box.style.backgroundColor = winColor : box.style.backgroundColor = "#232323"});
-            h1.style.background = winColor;
-            status.textContent = " Correct! ";
-            option.textContent = "Play Again?";
+            boxEvents(numSquares);
+            hoverEffect(option);
+            hoverEffect(easy);
         }else{
             this.style.backgroundColor = "#232323";
             status.textContent = "Try Again!"
